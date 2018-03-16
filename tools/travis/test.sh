cd716dd36792db4e40a14ee5d02b6d3dc8a8b1e7
Jim Crossley <jim@crossleys.org>
2018-03-16 12:51:55 -0400

    Test for activations betterer and "namespace" tests

--- tools/travis/test.sh
+++ tools/travis/test.sh
@@ -11,8 +11,8 @@ ROOTDIR="$SCRIPTDIR/../../"
 cd $ROOTDIR
 
 # To avoid hiding "admin bugs", do it as a guest
-# AUTH_SECRET=$(oc get secret whisk.auth -o yaml | grep "guest:" | awk '{print $2}' | base64 --decode)
-# wsk property set --auth $AUTH_SECRET --apihost $(oc get route/openwhisk --template={{.spec.host}})
+AUTH_SECRET=$(oc get secret whisk.auth -o yaml | grep "guest:" | awk '{print $2}' | base64 --decode)
+wsk property set --auth $AUTH_SECRET --apihost $(oc get route/openwhisk --template={{.spec.host}})
 
 invoke () {
   if [ -z "$1" ]; then
@@ -33,37 +33,58 @@ invoke () {
   echo "$1 invoked successfully"
 }
 
+waitForGreeting () {
+  NOW="$(date +%s)000"
+  FOUND=false
+  TIMEOUT=0
+  until [ $TIMEOUT -eq 10 ]; do
+    if [ -n "$(wsk -i activation list --since $NOW | grep greeting)" ]; then
+      FOUND=true
+      break
+    fi
+
+    let TIMEOUT=TIMEOUT+1
+    sleep 1
+  done
+
+  if [ "$FOUND" = false ]; then
+    echo "Failed to detect a greeting activation"
+    exit 1
+  fi
+}
+
 # Create actions
-wsk -i action create vars-py2 resources/vars.py --kind python:2
-wsk -i action create vars-py3 resources/vars.py --kind python:3
-wsk -i action create vars-js6 resources/vars.js --kind nodejs:6
-wsk -i action create vars-js8 resources/vars.js --kind nodejs:8
-wsk -i action create vars-java resources/vars.jar --main Vars
+wsk -i action create testsh-vars-py2 resources/vars.py --kind python:2
+wsk -i action create testsh-vars-py3 resources/vars.py --kind python:3
+wsk -i action create testsh-vars-js6 resources/vars.js --kind nodejs:6
+wsk -i action create testsh-vars-js8 resources/vars.js --kind nodejs:8
+wsk -i action create testsh-vars-java resources/vars.jar --main Vars
 
 # Invoke then delete them
 for i in {py2,py3,js6,js8,java}; do
-    invoke vars-$i
-    wsk -i action delete vars-$i
+    invoke testsh-vars-$i
+    wsk -i action delete testsh-vars-$i
 done
 
 # Fire a greeting every second
-wsk -i -d trigger create every-second \
+wsk -i trigger create testsh-every-second \
     --feed /whisk.system/alarms/alarm \
     --param cron '*/1 * * * * *' \
     --param trigger_payload "{\"name\":\"Odin\",\"place\":\"Asgard\"}"
-wsk -i rule create invoke-periodically every-second /whisk.system/samples/greeting
-# Wait for at least one greeting to fire
-sleep 3
-# Grab the id from the log -- there should only be one
-ACTIVATION_ID=$(oc logs --since=1s invoker-0 | grep "greeting.*activationId" | awk '{print $(NF-1)}')
+wsk -i rule create testsh-invoke-periodically testsh-every-second /whisk.system/samples/greeting
+# Account for stale reads in activation list
+waitForGreeting
+# Grab the id for the most recent greeting
+ID=$(wsk -i activation list | grep greeting | head -1 | awk '{print $1}')
 # Ensure we see our expected greeting
-RESULT=$(wsk -i activation get $ACTIVATION_ID | grep "Hello, Odin from Asgard!")
+RESULT=$(wsk -i activation get $ID | grep "Hello, Odin from Asgard!")
+# Clean up
+wsk -i rule    delete testsh-invoke-periodically
+wsk -i trigger delete testsh-every-second
+
 if [ -z "$RESULT" ]; then
     echo "FAILED! Unable to detect fired alarm trigger"
     exit 1
 fi
-# Clean up
-wsk -i rule    delete invoke-periodically
-wsk -i trigger delete every-second
 
 echo "PASSED! All actions invoked successfully"
