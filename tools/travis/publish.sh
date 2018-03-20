d61bbb8e278b9f19bf4a87021635043a36fe0f53
Jim Crossley <jim@crossleys.org>
2018-03-20 17:16:41 -0400

    Fix publish.sh to build/push all docker images [OCF-58]
    The deploy.sh had a confusing name, IMHO, so I got rid of it. At some
    point, we can build sha-tagged images for each successful travis build.

--- tools/travis/publish.sh
+++ tools/travis/publish.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 set -exu
 
-dockerhub_image_prefix="$1"
-dockerhub_image_name="$2"
-dockerhub_image_tag="$3"
-dir_to_build="$4"
-dockerhub_image="${dockerhub_image_prefix}/${dockerhub_image_name}:${dockerhub_image_tag}"
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+ROOTDIR="$SCRIPTDIR/../../"
+cd $ROOTDIR
 
-docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
-docker build ${dir_to_build} --tag ${dockerhub_image}
-docker push ${dockerhub_image}
+COMMIT=$(git rev-parse HEAD | cut -c 1-7)
+VERSION=${1:-$COMMIT}
+
+# docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
+
+publish() {
+  dockerhub_image_prefix="$1"
+  dockerhub_image_name="$2"
+  dockerhub_image_tag="$3"
+  dir_to_build="$4"
+  dockerhub_image="${dockerhub_image_prefix}/${dockerhub_image_name}:${dockerhub_image_tag}"
+
+  docker build ${dir_to_build} --tag ${dockerhub_image}
+  docker push ${dockerhub_image}
+}
+
+publish projectodd whisk_couchdb $VERSION docker/couchdb
+publish projectodd whisk_nginx $VERSION docker/nginx
+publish projectodd whisk_catalog $VERSION docker/catalog
+publish projectodd whisk_alarms $VERSION docker/alarms
+
+for i in $(ls docker/runtimes/); do
+  publish projectodd $i $VERSION docker/runtimes/$i
+done
